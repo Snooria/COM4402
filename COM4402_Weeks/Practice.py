d11f1901e9984ce3dbac0e6f7eb2de7e7a50ebff
shahana <snb1crt@bolton.ac.uk>
2025-12-17 23:10:48 +0000

    COM402_Flowchart_Pseudocode_txt

---
+++ COM4402_Weeks/Practice.py
@@ -0,0 +1,26 @@
+
+#name= str(input("Please enter name of product  "))
+#price = float(input("enter price: "))
+#quantity = int(input("quantity: "))
+#total_cost = float((price * quantity))
+#print("you bought", quantity, "of", name)
+#print("total cost is ", total_cost)
+
+#s = "    python is fun   "
+#s = s.rstrip()
+#print(s)
+
+
+#s = "hello world"
+#s = s.replace(" ","")
+#print(s)
+
+
+#text = "HELLO WORLD"
+#text = text.replace(" ","")
+#print(text.lower())
+
+name = str(input("enter your name: "))
+age = int(input("enter your age: "))
+course = str(input("enter the course name:  "))
+print(f"your name is {name}\nyou are {age} years old.\nyou are enrolled into {course}")
