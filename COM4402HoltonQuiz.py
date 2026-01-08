0b966fbedc077fea796e4abfb5ce7c23c25dceed
Snooria <94253876+Snooria@users.noreply.github.com>
2026-01-08 21:54:46 +0000

    Add files via upload

---
+++ COM4402HoltonQuiz.py
@@ -0,0 +1,23 @@
+# ---------------------Holton's Quiz Program-----------------------#
+
+
+count=0
+score=0
+
+print("--------------------Welcome to Holton's Quiz-------------------")
+choice= str(input("options:\nA.Login\nB.Login\nChoose the option: "))
+
+
+# choice= str(input("Choose the option:\nA.Login \n B.Signup")
+
+# if choice==a then prompt to login or else sign up.
+if choice == "A":
+    print("Please enter your name")
+    print("Please enter your password")
+else:
+    signup= str(input("Please enter your name and password to sign up"))
+
+
+
+
+
