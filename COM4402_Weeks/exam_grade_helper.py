88c2234abb91af1eafa2b1d757fd6800a629ea0e
shahana <sh_nooria@yahoo.com>
2026-01-13 15:42:51 +0000

    w4s1 exam grade

--- COM4402_Weeks/exam_grade_helper.py
+++ COM4402_Weeks/exam_grade_helper.py
@@ -1,3 +1,68 @@
 def classify_mark(mark):
-    return "Fail"
+    if not isinstance(mark, int):
+        raise TypeError("Invalid type")
+    if mark < 0 or mark > 100:
+        raise ValueError
+    if mark<=39:
+         return"Fail"
+    if mark>=40 and mark<=69:
+         return"Pass"
+    if mark>=70 & mark<=100:
+            return"Distinction"
+    else:
+        print("Error")
+        return int(mark)
+#-------------------------------#
+
+
+fail_count=0
+distinction_count=0
+
+def calculate_summary(marks):
+    if len(marks) == 0:
+        return [0, 0.0, 0, 0]
+
+    total=0
+    fail_count = 0
+    distinction_count = 0
+
+    for mark in marks:
+        if mark < 0 or mark > 100:
+            raise ValueError
+        total+= mark
+        if mark < 40:
+            fail_count += 1
+        if mark >= 70:
+            distinction_count += 1
+
+    average=0.0
+    average = total / len(marks)
+
+
+    return [total, average, fail_count, distinction_count]
+
+
+
+
+
+
+
+
+
+
+
+#------------------------#
+
+
+
+
+
+
+
+
+
+
+
+
+
 
